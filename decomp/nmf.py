cbbd93bc7d87b4d28d37193c802c73bbe35c94a9
fujiisoup <fujiisoup@gmail.com>
2017-11-16 22:07:34 +0900

    Add kl loss.

--- decomp/nmf.py
+++ decomp/nmf.py
@@ -1,15 +1,17 @@
 import numpy as np
 from .utils.cp_compat import get_array_module
-from .utils.dtype import float_type
 from .utils.data import minibatch_index
 from .utils import assertion, normalize
 
 
-_JITTER = 1.0e-10
+AVAILABLE_METHODS = ['multiplicative', 'spgd']
+_JITTER = 1.0e-15
 
 
-def solve(y, D, x=None, tol=1.0e-3, minibatch=None, maxiter=1000,
-          likelihood='square', mask=None, random_seed=None):
+def solve(y, D, x=None, tol=1.0e-3,
+          minibatch=None, maxiter=1000, method='multiplicative',
+          likelihood='l2', minibatch_iter=100, mask=None,
+          random_seed=None):
     """
     Non-negative matrix factrization.
 
@@ -27,24 +29,20 @@ def solve(y, D, x=None, tol=1.0e-3, minibatch=None, maxiter=1000,
         Shape: [n_samples, ch]
     D: array-like.
         Initial dictionary, shape [ch, n_component]
-    alpha: a positive float
-        Regularization parameter
     x0: array-like
         An initial estimate of x
-
     tol: a float.
-        Criterion
+        Criterion to stop iteration
+    maxiter: an integer
+        Number of iteration
+    method: string
+        One of AVAILABLE_METHODS
+    likelihood: string
+        One of ['l2']
 
     mask: an array-like of Boolean (or integer, float)
         The missing point should be zero. One for otherwise.
 
-    Notes
-    -----
-    This is essentially implements
-    Mensch ARTHURMENSCH, A., Mairal JULIENMAIRAL, J., & Thirion BETRANDTHIRION,
-    B. (n.d.).
-    Dictionary Learning for Massive Matrix Factorization Gael Varoquaux.
-    Retrieved from http://proceedings.mlr.press/v48/mensch16.pdf
     """
 
     # Check all the class are numpy or cupy
@@ -62,49 +60,165 @@ def solve(y, D, x=None, tol=1.0e-3, minibatch=None, maxiter=1000,
     assertion.assert_ndim('y', y, 2)
     assertion.assert_ndim('D', D, 2)
     assertion.assert_ndim('x', x, 2)
+    assertion.assert_nonnegative(D)
+    assertion.assert_nonnegative(x)
 
-    return solve_fastpath(y, D, x, tol, minibatch, maxiter, likelihood,
-                          rng, xp, mask)
+    if likelihood not in ['l2']:
+        assertion.assert_nonnegative(y)
 
-
-def solve_fastpath(y, D, x, tol, minibatch, maxiter,
-                   likelihood, rng, xp, mask):
-    """ Fast path for NMF """
     D = normalize.l2_strict(D, axis=-1, xp=xp)
-    if mask is None:
-        if likelihood == 'square':
-            return solve_square(y, D, x, tol, minibatch, maxiter, rng, xp)
+    if method == 'multiplicative':
+        if mask is None:
+            gradients_x = {'l2': _grad_x_l2,
+                           'kl': _grad_x_kl,
+                           'poisson', _grad_x_kl}
+            gradients_d = {'l2': _grad_d_l2,
+                           'kl': _grad_d_kl,
+                           'poisson', _grad_d_kl}
         else:
-            raise NotImplementedError('NMF with {0:s} likelihood is not yet '
-                                      'implemented'.format(likelihood))
+            gradients_x = {'l2': _grad_x_l2_mask,
+                           'kl': _grad_x_kl_mask,
+                           'poisson', _grad_x_kl_mask}
+            gradients_d = {'l2': _grad_d_l2_mask,
+                           'kl': _grad_d_kl_mask,
+                           'poisson', _grad_d_kl_mask}
+
+        if minibatch is None:
+            return multiplicative_fullbatch(y, D, x, tol, maxiter, mask, xp,
+                                            grad_x=gradients_x[likelihood],
+                                            grad_D=gradients_d[likelihood])
+        else:
+            raise NotImplementedError('NMF with minibatch is not yet '
+                                      'implemented.')
+            return multiplicative_minibatch(
+                y, D, x, tol, maxiter, mask, minibatch, minibatch_iter,
+                rng, xp,
+                grad_x=gradients_x[likelihood],
+                grad_D=gradients_d[likelihood])
     else:
-        raise NotImplementedError('NMF with mask is not yet implemented.')
+        raise NotImplementedError('NMF with {} algorithm is not yet '
+                                  'implemented.'.format(method))
 
 
-def solve_square(y, D, x, tol, minibatch, maxiter, rng, xp):
-    """ NMF with square sum likelihood """
-    xx_sum = xp.zeros((D.shape[0], D.shape[0]), dtype=y.dtype)
-    xy_sum = xp.zeros((D.shape[0], D.shape[1]), dtype=y.dtype)
+# --- l2 loss ---
+def _grad_x_l2(y, x, d, mask, xp):
+    """ Multiplicative update rule for square loss.
+    Returns Positive (numerator) and negative (denominator).
+    mask is not used.
+    """
+    f = xp.dot(x, d)
+    return xp.dot(y, d.T), xp.dot(f, d.T)
+
+
+def _grad_d_l2(y, x, d, mask, xp):
+    """ update d with l2 loss """
+    f = xp.dot(x, d)
+    return xp.dot(x.T, y), xp.dot(x.T, f)
+
+
+def _grad_x_l2_mask(y, x, d, mask, xp):
+    """ Multiplicative update rule for square loss.
+    Returns Positive (numerator) and negative (denominator).
+    mask is not used.
+    """
+    f = xp.dot(x, d) * mask
+    y = y * mask
+    return xp.dot(y, d.T), xp.dot(f, d.T)
+
+
+def _grad_d_l2_mask(y, x, d, mask, xp):
+    """ update d with l2 loss """
+    f = xp.dot(x, d) * mask
+    y = y * mask
+    return xp.dot(x.T, y), xp.dot(x.T, f)
+
+
+# --- KL loss ---
+def _grad_x_kl(y, x, d, mask, xp):
+    """ Multiplicative update rule for KL loss.    """
+    f = xp.dot(x, d) + _JITTER
+    return xp.dot(y / f, d.T), xp.sum(d.T, axis=0, keepdims=True)
+
+
+def _grad_d_kl(y, x, d, mask, xp):
+    """ update d with KL loss """
+    f = xp.dot(x, d) + _JITTER
+    return xp.dot(x.T, y / f), xp.sum(x.T, axis=1, keepdims=True)
+
+
+def _grad_x_kl_mask(y, x, d, mask, xp):
+    """ Multiplicative update rule for KL loss with mask. """
+    f = xp.dot(x, d) + _JITTER
+    y = y * mask
+    return xp.dot(y / f, d.T), xp.dot(mask, d.T)
+
+
+def _grad_d_kl_mask(y, x, d, mask, xp):
+    """ update d with l2 loss """
+    f = xp.dot(x, d) + _JITTER
+    y = y * mask
+    return xp.dot(x.T, y / f), xp.dot(x.T, mask)
+
+
+def multiplicative_fullbatch(y, D, x, tol, maxiter, mask, xp, grad_x, grad_D):
+    """ NMF with fullbatch update """
+    for it in range(1, maxiter):
+        # update x
+        grad_x_pos, grad_x_neg = grad_x(y, x, D, mask, xp)
+        x = x * xp.maximum(grad_x_pos, 0.0) / xp.maximum(grad_x_neg, _JITTER)
+        # update D
+        grad_D_pos, grad_D_neg = grad_D(y, x, D, mask, xp)
+        U = D * xp.maximum(grad_D_pos, 0.0) / xp.maximum(grad_D_neg, _JITTER)
+        D_new = normalize.l2_strict(U, axis=-1, xp=xp)
+        if it % 100 == 0:
+            print(xp.max(xp.abs(D - D_new)))
+
+        if xp.max(xp.abs(D - D_new)) < tol:
+            return it, D_new, x
+        D = D_new
+
+    return maxiter, D, x
+
+
+def multiplicative_minibatch(y, D, x, tol, maxiter, mask, minibatch,
+                             minibatch_iter, rng, xp, grad_x, grad_D):
+    """ NMF with minibatch update
+    from
+    """
+    forget_rate = 0.1
+    grad_D_pos_sum = xp.zeros_like(D)
+    grad_D_neg_sum = xp.zeros_like(D)
+
+    def accumurate_grad(grad_sum, grad):
+        return (1.0 - forget_rate) * grad_sum + forget_rate * grad
 
     for it in range(1, maxiter):
         indexes = minibatch_index(y.shape[:1], minibatch, rng)
         x_minibatch = x[indexes]
         y_minibatch = y[indexes]
+        mask_minibatch = mask[indexes] if mask is not None else None
         # update x
-        ymu = xp.dot(x_minibatch, D)
-        x_minibatch = x_minibatch * (xp.dot(y_minibatch, D.T) /
-                                     xp.dot(ymu, D.T))
+        for minibatch_it in range(minibatch_iter):
+            grad_x_pos, grad_x_neg = grad_x(
+                            y_minibatch, x_minibatch, D, mask_minibatch, xp)
+            x_minibatch = x_minibatch * \
+                xp.maximum(grad_x_pos, 0.0) / xp.maximum(grad_x_neg, _JITTER)
         x[indexes] = x_minibatch
 
-        xx_sum += xp.dot(x_minibatch.T, x_minibatch) / it
-        xy_sum += xp.dot(x_minibatch.T, y_minibatch) / it
-
         # update D
-        U = D * xy_sum / xp.dot(xx_sum, D)
-        D_new = normalize.l2(U, axis=-1, xp=xp)
+        grad_D_pos, grad_D_neg = grad_D(y_minibatch, x_minibatch, D,
+                                        mask_minibatch, xp)
+        grad_D_pos_sum = accumurate_grad(grad_D_pos_sum, grad_D_pos)
+        grad_D_neg_sum = accumurate_grad(grad_D_neg_sum, grad_D_neg)
 
+        U = D * xp.maximum(grad_D_pos_sum, 0.0) / xp.maximum(grad_D_neg_sum,
+                                                             _JITTER)
+        D_new = normalize.l2_strict(U, axis=-1, xp=xp)
         if xp.max(xp.abs(D - D_new)) < tol:
             return it, D, x
         D = D_new
 
+        if it % 10 == 0:
+            #print(grad_D_pos_sum[0, :4])
+            print(xp.sum(xp.square(y - xp.dot(x, D))))
     return maxiter, D, x
--- tests/test_nmf.py
+++ tests/test_nmf.py
@@ -1,29 +1,37 @@
 import unittest
 from decomp.utils.cp_compat import numpy_or_cupy as xp
+from decomp.utils import normalize
 import decomp.nmf as nmf
 from .testings import allclose
 
 
-class TestFloat(unittest.TestCase):
+class Test_L2(unittest.TestCase):
     def randn(self, *shape):
         return self.rng.randn(*shape)
 
+    @property
+    def likelihood(self):
+        return 'l2'
+
+    def get_y(self, x, d):
+        ytrue = xp.dot(x, d)
+        return ytrue + self.randn(*ytrue.shape) * 0.1
+
     def setUp(self):
         self.rng = xp.random.RandomState(0)
-        self.Dtrue = xp.maximum(self.randn(3, 5), 0.0)
+        self.Dtrue = xp.maximum(self.randn(3, 20), 0.0)
         self.xtrue = xp.maximum(self.randn(101, 3), 0.0)
 
-        self.y = xp.dot(self.xtrue, self.Dtrue) + self.randn(101, 5) * 0.1
+        self.y = self.get_y(self.xtrue, self.Dtrue)
         self.D = xp.maximum(self.Dtrue + self.randn(*self.Dtrue.shape) * 0.3,
-                            0.0)
+                            0.1)
         self.mask = xp.rint(
-                self.rng.uniform(0.3, 1, size=505)).reshape(101, 5)
+            self.rng.uniform(0.3, 1, size=self.y.size)).reshape(self.y.shape)
 
-    def error(self, x, D, mask=None):
-        mask = xp.ones(self.y.shape, dtype=float) if mask is None else mask
-        D = D / xp.maximum(xp.sum(D * D, axis=0), 1.0)
-        loss = xp.sum(xp.square(xp.abs(
-            self.y - xp.tensordot(x, D, axes=1))))
+    def error(self, x, D, mask):
+        mask = xp.ones(self.y.shape, self.y.dtype) if mask is None else mask
+        D = normalize.l2_strict(D, axis=-1, xp=xp)
+        loss = xp.sum(xp.square(self.y - xp.dot(x, D)) * mask)
         return 0.5 * loss
 
     def assert_minimum(self, x, D, tol, n=100, mask=None):
@@ -32,27 +40,51 @@ class TestFloat(unittest.TestCase):
             dx = self.randn(*x.shape) * tol
             dD = self.randn(*D.shape) * tol
             assert loss < self.error(xp.maximum(x + dx, 0.0),
-                                     xp.maximum(D + dD, 0.0), mask) + tol
+                                     xp.maximum(D + dD, 0.0), mask) + 1.0e-15
 
     @property
-    def likelihood(self):
-        return 'square'
+    def maxiter(self):
+        return 3000
+
+    @property
+    def minibatch_maxiter(self):
+        return 1000
 
     def test_run(self):
         D = self.D.copy()
-        it, D, x = nmf.solve(self.y, D, minibatch=None, maxiter=1000,
-                             tol=1.0e-6, random_seed=0)
-        assert it < 1000 - 1
-        print(self.D)
+        it, D, x = nmf.solve(self.y, D, x=None, tol=1.0e-6,
+                             minibatch=None, maxiter=self.maxiter,
+                             method='multiplicative',
+                             likelihood=self.likelihood, mask=None,
+                             random_seed=0)
+        assert it < self.maxiter - 1
         self.assert_minimum(x, D, tol=1.0e-5, n=100)
         assert not allclose(D, xp.zeros_like(D), atol=1.0e-5)
 
+    def test_run_mask(self):
+        D = self.D.copy()
+        it, D, x = nmf.solve(self.y, D, x=None, tol=1.0e-6,
+                             minibatch=None, maxiter=self.maxiter,
+                             method='multiplicative',
+                             likelihood=self.likelihood, mask=self.mask,
+                             random_seed=0)
+        assert it < self.maxiter - 1
+        self.assert_minimum(x, D, tol=1.0e-5, n=100, mask=self.mask)
+        assert not allclose(D, xp.zeros_like(D), atol=1.0e-5)
+
+    def _test_run_minibatch(self):
         D_minibatch = self.D.copy()
         it, D_minibatch, x_minibatch = nmf.solve(
-            self.y, D, minibatch=10, maxiter=1000, tol=1.0e-6, random_seed=0)
-
-        assert it < 1000 - 1
-        assert allclose(D, D_minibatch, atol=1.0e-5)
+                     self.y, D_minibatch, x=None, tol=1.0e-3,
+                     minibatch=10, maxiter=self.minibatch_maxiter,
+                     minibatch_iter=1,
+                     method='multiplicative',
+                     likelihood=self.likelihood, mask=None,
+                     random_seed=0)
+        assert it < self.minibatch_maxiter - 1
+        print(D)
+        print(D - D_minibatch)
+        assert allclose(D, D_minibatch, atol=1.0e-2)
 
     '''
     def test_run_minibatch_mask(self):
@@ -68,6 +100,23 @@ class TestFloat(unittest.TestCase):
                                 tol=1.0e-6, mask=None, random_seed=0)
         self.assertFalse(allclose(D, D2, atol=1.0e-4))
     '''
+
+
+class Test_KL(Test_L2):
+    @property
+    def likelihood(self):
+        return 'kl'
+
+    def get_y(self, x, d):
+        ytrue = xp.dot(x, d)
+        return ytrue + xp.abs(self.randn(*ytrue.shape) * 0.1)
+
+    def error(self, x, D, mask):
+        mask = xp.ones(self.y.shape, self.y.dtype) if mask is None else mask
+        D = normalize.l2_strict(D, axis=-1, xp=xp)
+        f = xp.maximum(xp.dot(x, D), 1.0e-15)
+        return xp.sum((self.y * xp.log(xp.maximum(self.y, 1.0e-15) / f)
+                      + f) * mask)
 
 
 if __name__ == '__main__':
